e390ba5df509bd1265b1c5f625f2b07097574b7d
Richard T <richard@turing.edu>
2023-04-19 15:50:13 -0400

    Test for AddSong method

--- RecordLabel.UnitTests/AlbumTest.cs
+++ RecordLabel.UnitTests/AlbumTest.cs
@@ -14,5 +14,48 @@ namespace RecordLabel.UnitTests
         }
 
         // You will write additional tests for the Record class, and the Inventory Class.
+        [Fact]
+        public void Album_AddSong_AddsSongToCorrectList()
+        {
+            // Arrange
+            Album stopMakingSense = new Album("Talking Heads", "Stop Making Sense");
+
+            // Act: add 4 songs
+            stopMakingSense.AddSong("Song 1");
+            stopMakingSense.AddSong("Song 2");
+            stopMakingSense.AddSong("Song 3");
+            stopMakingSense.AddSong("Song 4");
+
+            // Assert: Side A 4, Side B 0
+            Assert.Equal(4, stopMakingSense.SideASongs.Count);
+            Assert.Empty(stopMakingSense.SideBSongs);
+
+            // Act: add 4 more songs
+            stopMakingSense.AddSong("Song 5");
+            stopMakingSense.AddSong("Song 6");
+            stopMakingSense.AddSong("Song 7");
+            stopMakingSense.AddSong("Song 8");
+
+            // Assert: Side A 5, Side B 3
+            Assert.Equal(5, stopMakingSense.SideASongs.Count);
+            Assert.Equal(3, stopMakingSense.SideBSongs.Count);
+
+            // Act: add 2 more songs
+            stopMakingSense.AddSong("Song 9");
+            stopMakingSense.AddSong("Song 10");
+
+            // Assert: Side A 5, Side B 5
+            Assert.Equal(5, stopMakingSense.SideASongs.Count);
+            Assert.Equal(5, stopMakingSense.SideBSongs.Count);
+
+            // Act: add 2 more songs
+            stopMakingSense.AddSong("Song 11");
+            stopMakingSense.AddSong("Song 12");
+
+            // Assert: Side A 5, Side B 5, none added
+            Assert.Equal(5, stopMakingSense.SideASongs.Count);
+            Assert.Equal(5, stopMakingSense.SideBSongs.Count);
+        }
+
     }
 }
